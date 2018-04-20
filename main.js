369e7b1904c665ec564d2c885c3cc63a224f758c
Gary Anderson <ganderson@seattleacademy.org>
2018-04-20 06:29:42 -0700

    fix working with socket.io

--- main.js
+++ main.js
@@ -16,11 +16,11 @@ console.log("main.js");
 
 
 
-import {KNNImageClassifier} from 'deeplearn-knn-image-classifier';
+import { KNNImageClassifier } from 'deeplearn-knn-image-classifier';
 import * as dl from 'deeplearn';
 
 // Number of classes to classify
-const NUM_CLASSES = 3;
+const NUM_CLASSES = 4;
 // Webcam Image size. Must be 227. 
 const IMAGE_SIZE = 227;
 // K value for KNN
@@ -28,125 +28,139 @@ const TOPK = 10;
 
 
 class Main {
-  constructor(){
-    // Initiate variables
-    this.infoTexts = [];
-    this.training = -1; // -1 when no class is being trained
-    this.videoPlaying = false;
-    
-    // Initiate deeplearn.js math and knn classifier objects
-    this.knn = new KNNImageClassifier(NUM_CLASSES, TOPK);
-    
-    // Create video element that will contain the webcam image
-    this.video = document.createElement('video');
-    this.video.setAttribute('autoplay', '');
-    this.video.setAttribute('playsinline', '');
-    
-    // Add video element to DOM
-    document.body.appendChild(this.video);
-    
-    // Create training buttons and info texts    
-    for(let i=0;i<NUM_CLASSES; i++){
-      const div = document.createElement('div');
-      document.body.appendChild(div);
-      div.style.marginBottom = '10px';
+    constructor() {
+        // Initiate variables
+        this.infoTexts = [];
+        this.training = -1; // -1 when no class is being trained
+        this.videoPlaying = false;
 
-      // Create training button
-      const button = document.createElement('button')
-      button.innerText = "Train "+i;
-      div.appendChild(button);
+        // Initiate deeplearn.js math and knn classifier objects
+        this.knn = new KNNImageClassifier(NUM_CLASSES, TOPK);
 
-      // Listen for mouse events when clicking the button
-      button.addEventListener('mousedown', () => this.training = i);
-      button.addEventListener('mouseup', () => this.training = -1);
-      
-      // Create info text
-      const infoText = document.createElement('span')
-      infoText.innerText = " No examples added";
-      div.appendChild(infoText);
-      this.infoTexts.push(infoText);
+        // Create video element that will contain the webcam image
+        this.video = document.createElement('video');
+        this.video.setAttribute('autoplay', '');
+        this.video.setAttribute('playsinline', '');
+
+        // Add video element to DOM
+        document.body.appendChild(this.video);
+
+        // Create training buttons and info texts    
+        for (let i = 0; i < NUM_CLASSES; i++) {
+            const div = document.createElement('div');
+            document.body.appendChild(div);
+            div.style.marginBottom = '10px';
+
+            // Create training button
+            const button = document.createElement('button')
+            button.innerText = "Train " + i;
+            div.appendChild(button);
+
+            // Listen for mouse events when clicking the button
+            button.addEventListener('mousedown', () => this.training = i);
+            button.addEventListener('mouseup', () => this.training = -1);
+
+            // Create info text
+            const infoText = document.createElement('span')
+            infoText.innerText = " No examples added";
+            div.appendChild(infoText);
+            this.infoTexts.push(infoText);
+        }
+
+
+        // Setup webcam
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then((stream) => {
+                this.video.srcObject = stream;
+                this.video.width = IMAGE_SIZE;
+                this.video.height = IMAGE_SIZE;
+
+                this.video.addEventListener('playing', () => this.videoPlaying = true);
+                this.video.addEventListener('paused', () => this.videoPlaying = false);
+            })
+
+        // Load knn model
+        this.knn.load()
+            .then(() => this.start());
     }
-    
-    
-    // Setup webcam
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    .then((stream) => {
-      this.video.srcObject = stream;
-      this.video.width = IMAGE_SIZE;
-      this.video.height = IMAGE_SIZE;
 
-      this.video.addEventListener('playing', ()=> this.videoPlaying = true);
-      this.video.addEventListener('paused', ()=> this.videoPlaying = false);
-    })
-    
-    // Load knn model
-    this.knn.load()
-    .then(() => this.start()); 
-  }
-  
-  start(){
-    if (this.timer) {
-      this.stop();
+    start() {
+        if (this.timer) {
+            this.stop();
+        }
+        this.video.play();
+        this.timer = requestAnimationFrame(this.animate.bind(this));
     }
-    this.video.play();
-    this.timer = requestAnimationFrame(this.animate.bind(this));
-  }
-  
-  stop(){
-    this.video.pause();
-    cancelAnimationFrame(this.timer);
-  }
-  
-  animate(){
-    if(this.videoPlaying){
-      // Get image data from video element
-      const image = dl.fromPixels(this.video);
-      
-      // Train class if one of the buttons is held down
-      if(this.training != -1){
-        // Add current image to classifier
-        this.knn.addImage(image, this.training)
-      }
-      
-      // If any examples have been added, run predict
-      const exampleCount = this.knn.getClassExampleCount();
-      const debugmessage = document.getElementById('debugmessage');
-      debugmessage.innerText = "--";
-      if(Math.max(...exampleCount) > 0){
-        this.knn.predictClass(image)
-        .then((res)=>{
-          for(let i=0;i<NUM_CLASSES; i++){
-            // Make the predicted class bold
-            if(res.classIndex == i){
-              this.infoTexts[i].style.fontWeight = 'bold';
+
+    stop() {
+        this.video.pause();
+        cancelAnimationFrame(this.timer);
+    }
+
+    animate() {
+        if (this.videoPlaying) {
+            // Get image data from video element
+            const image = dl.fromPixels(this.video);
+
+            // Train class if one of the buttons is held down
+            if (this.training != -1) {
+                // Add current image to classifier
+                this.knn.addImage(image, this.training)
+            }
+
+            // If any examples have been added, run predict
+            const exampleCount = this.knn.getClassExampleCount();
+            const debugmessage = document.getElementById('debugmessage');
+            debugmessage.innerText = "--";
+            if (Math.max(...exampleCount) > 0) {
+                this.knn.predictClass(image)
+                    .then((res) => {
+                        for (let i = 0; i < NUM_CLASSES; i++) {
+                            // Make the predicted class bold
+                            if (res.classIndex == i) {
+                                this.infoTexts[i].style.fontWeight = 'bold';
+                            } else {
+                                this.infoTexts[i].style.fontWeight = 'normal';
+                            }
+
+                            // Update info text
+                            if (exampleCount[i] > 0) {
+                                this.infoTexts[i].innerText = ` ${exampleCount[i]} examples - ${res.confidences[i]*100}%`
+                            }
+
+                            if (i == 0 && res.confidences[0] >= .9) {
+                                debugmessage.innerText = 'stop';
+                               // socket.emit("/dance/dance1", "stop");
+                            }
+
+                            if (i == 1 && res.confidences[1] >= .9) {
+                                debugmessage.innerText = 'flip';
+                                socket.emit("/dance/dance1", "flip");
+                                //socket.emit('/message', 'flip');
+                            }
+
+                            if (i == 2 && res.confidences[2] >= 1) {
+                                debugmessage.innerText = 'land';
+                                socket.emit("/pilot/drone", { action : 'land'});
+                            }
+
+
+                            if (i == 3 && res.confidences[3] >= 1) {
+                                debugmessage.innerText = 'sissors';
+                                socket.emit('/move', 'land');
+                            }
+
+
+                        }
+                    })
+                    // Dispose image when done
+                    .then(() => image.dispose())
             } else {
-              this.infoTexts[i].style.fontWeight = 'normal';
+                image.dispose()
             }
-
-            // Update info text
-            if(exampleCount[i] > 0){
-              this.infoTexts[i].innerText = ` ${exampleCount[i]} examples - ${res.confidences[i]*100}%`
-            }
-
-            if(i == 0 && res.confidences[0] > .9){
-              debugmessage.innerText = 'stop';
-            }
-
-            if(i == 1 && res.confidences[1] > .9){
-              debugmessage.innerText = 'flip';
-              socket.emit('/message', 'flip');
-            }
-
-          }
-        })
-        // Dispose image when done
-        .then(()=> image.dispose())
-      } else {
-        image.dispose()
-      }
+        }
+        this.timer = requestAnimationFrame(this.animate.bind(this));
     }
-    this.timer = requestAnimationFrame(this.animate.bind(this));
-  }
 }
 
 window.addEventListener('load', () => new Main());
